122f571daffb718ddc4427fbe0491f32c4b82238
alexdolya <alexdolya90@gmail.com>
2024-04-16 15:20:24 +0400

    initial

---
+++ metrics-consumer/src/test/java/ru/dolya/metricsconsumer/MetricsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolya.metricsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
